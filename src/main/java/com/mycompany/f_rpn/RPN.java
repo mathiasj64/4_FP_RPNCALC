81437397e0527e80a0a9defa4f0b9064160d3966
Mathias <dudi.achmed@yahoo.dk>
2017-02-06 12:12:57 +0100

    Added the whole thing

---
+++ src/main/java/com/mycompany/f_rpn/RPN.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.f_rpn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mathias
+ */
+public class RPN
+{
+
+    public static String inputs;
+
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println("Welcome to the RPN calculator - type arguments to begin");
+        run();
+    }
+
+    public static void run() throws IOException
+    {
+        Stack stack = new Stack();
+        while (true)
+        {
+            System.out.print(">");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            inputs = br.readLine();
+
+            if (!inputs.isEmpty())
+            {
+
+                String[] inputList = inputs.split(" ");
+
+                for (String input : inputList)
+                {
+                    if (stack.isMathematicalOperator(input) || stack.isNumeric(input))
+                    {
+                        if (stack.isMathematicalOperator(input))
+                        {
+                            stack.push(input);
+                        } else
+                        {
+                            stack.push(Integer.parseInt(input));
+                        }
+                    }
+                stack.calculate();
+                }
+            }
+
+            System.out.println(stack.toString() + " ");
+        }
+    }
+
+}
---
+++ src/main/java/com/mycompany/f_rpn/Stack.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.f_rpn;
+
+/**
+ *
+ * @author Mathias
+ */
+public class Stack<T>
+{
+
+    public T[] data = null;
+    public int top = 0;
+
+    public Stack()
+    {
+        this.data = (T[]) new Object[100];
+    }
+
+    public void push(T element)
+    {
+        data[top] = element;
+        top++;
+    }
+
+    public T pop() throws Exception
+    {
+        if (isEmpty())
+        {
+            throw new Exception("Unable to pop empty stack");
+        }
+        T element = data[top];
+        top--;
+        return element;
+    }
+
+    public boolean isEmpty()
+    {
+        return top == 0;
+    }
+
+    @Override
+    public String toString()
+    {
+        if (!isEmpty())
+        {
+            String result = "";
+            for (int i = 0; i < top; i++)
+            {
+                if (data[i] != null)
+                {
+                    result += data[i] + " ";
+                }
+            }
+            return result;
+        }
+        return ("Stack is empty");
+    }
+
+    public void calculate()
+    {
+        if (top >= 3)
+        {
+            if (isMathematicalOperator(data[top - 1].toString()))
+            {
+                if (data[top - 1].equals("+"))
+                {
+                    Integer res = Integer.parseInt(data[top - 3].toString()) + Integer.parseInt(data[top - 2].toString());
+
+                    top = top - 3;
+                    push((T) res);
+                }
+                
+                if (data[top - 1].equals("-"))
+                {
+                    Integer res = Integer.parseInt(data[top - 3].toString()) - Integer.parseInt(data[top - 2].toString());
+
+                    top = top - 3;
+                    push((T) res);
+                }
+                
+                if (data[top - 1].equals("*"))
+                {
+                    Integer res = Integer.parseInt(data[top - 3].toString()) * Integer.parseInt(data[top - 2].toString());
+
+                    top = top - 3;
+                    push((T) res);
+                }
+                
+                if (data[top - 1].equals("/"))
+                {
+                    Integer res = Integer.parseInt(data[top - 3].toString()) / Integer.parseInt(data[top - 2].toString());
+
+                    top = top - 3;
+                    push((T) res);
+                }
+            }
+        }
+    }
+
+    public boolean isMathematicalOperator(String str)
+    {
+        return(str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/"));
+    }
+
+    public boolean isNumeric(String str)
+    {
+        for (char c : str.toCharArray())
+        {
+            if (!Character.isDigit(c))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
